4171f3e38a75c08a14b94eee83e4f68df52a5392
pandaman64 <kointosudesuyo@infoseek.jp>
2023-02-18 15:59:08 +0900

    Set up dev Nix environment

--- flake.nix
+++ flake.nix
@@ -2,17 +2,17 @@
   description = "A query language for serde data types";
 
   outputs = { self, nixpkgs }:
-  let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in
-  {
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      packages = [
-        pkgs.nixpkgs-fmt
-        pkgs.nil
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = [
+          pkgs.nixpkgs-fmt
+          pkgs.nil
 
-        pkgs.rustup
-      ];
+          pkgs.rustup
+        ];
+      };
     };
-  };
 }
