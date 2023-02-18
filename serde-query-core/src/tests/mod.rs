728bd1e9c762b3412b6cc43c3e179f7cefe9207e
pandaman64 <kointosudesuyo@infoseek.jp>
2023-02-18 21:02:41 +0900

    E2E snapshot tests

--- serde-query-core/src/tests/mod.rs
+++ serde-query-core/src/tests/mod.rs
@@ -1,1 +1,18 @@
+use syn::DeriveInput;
+
+use crate::{generate_derive, DeriveTarget};
+
 mod generate_basic;
+
+fn snapshot_derive(input: DeriveInput, target: DeriveTarget) -> String {
+    match generate_derive(input, target) {
+        Ok(stream) => prettyplease::unparse(&syn::parse2(stream).unwrap()),
+        Err(diagnostics) => {
+            let diagnostics: Vec<String> = diagnostics
+                .into_iter()
+                .map(|diagnostic| format!("{:?}", diagnostic))
+                .collect();
+            diagnostics.join("\n")
+        }
+    }
+}
