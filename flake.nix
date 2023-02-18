39664bed9c889c4f9208f58680a76e967f32d471
pandaman64 <kointosudesuyo@infoseek.jp>
2023-02-18 15:50:36 +0900

    Add flakes

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "A query language for serde data types";
+
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = [
+        pkgs.nixpkgs-fmt
+        pkgs.nil
+
+        pkgs.rustup
+      ];
+    };
+  };
+}
